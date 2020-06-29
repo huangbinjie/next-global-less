004c6d01d754272ddae57c1041653850eaf8373e
黄斌杰 <akira.binjie@gmail.com>
2020-06-29 15:46:58 +0800

    0.0.2

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
-const path = require('path')
-
 module.exports = ({ globalPath, webpack, ...nextConfig } = {}) => ({
   ...nextConfig,
 
   webpack: (config, options) => {
-    // 默认的 less 规则里面过滤掉对指定的 globalPath 的处理
-    config.module.rules.find((rule) => String(rule.test) === String(/\.less$/)).exclude = [
-      path.join(__dirname, globalPath)
-    ]
+    if (!options.defaultLoaders) {
+      throw new Error(
+        'This plugin is not compatible with Next.js versions below 5.0.0 https://err.sh/next-plugins/upgrade'
+      )
+    }
 
-    // copy 一份 less 规则给 global.less 用
+    if (!options.defaultLoaders.less) {
+      throw new Error('Cannot find less config, make sure next-less is working correctly.')
+    }
+
+    // next-less 处理够会往 webpack 配置里面 push 这样的规则，找到并过滤掉对指定的 globalPath 的处理
+    config.module.rules.find((rule) => String(rule.test) === String(/\.less$/)).exclude = globalPath
+
+    // copy 一份 less loader 给 global.less 用
     const ignoreGlobalLessLoaders = JSON.parse(JSON.stringify(options.defaultLoaders.less))
 
-    // 这条规则有多个 loader，css-loader 是其中之一，并且 cssModules 是其控制
+    // 这个 loader 有多个子 loader，css-loader 是其中之一，并且 cssModules 是其控制
     // 找到它并关闭 cssModules
     ignoreGlobalLessLoaders.find(
       (loader) => typeof loader === 'object' && loader.loader.indexOf('css-loader') > -1
     ).options.modules = false
 
     config.module.rules.push({
-      test: new RegExp(path.join(__dirname, globalPath) + '$'),
+      test: new RegExp(escapePath(globalPath)),
       use: ignoreGlobalLessLoaders
     })
 
@@ -30,3 +36,7 @@ module.exports = ({ globalPath, webpack, ...nextConfig } = {}) => ({
     return config
   }
 })
+
+function escapePath(path) {
+  return path.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d')
+}
--- readme.md
+++ readme.md
@@ -1,12 +1,19 @@
 # next-global-less
 
-在开启 css modules 的 less 项目中，指定一个文件让其不被 css modules 处理。
+在被 `next-less` 处理过且开启 css modules 的 less 项目中，指定一个文件让其不被 css modules 处理。
 
+
+## Install
+
+```sh
+yarn add next-global-less
+```
 
 ## Usage
 
 ```js
 const withGlobalLess = require('./next-global-less')
+const path = require('path')
 
-module.exports = withGlobalLess({globalPath: './pages/global.less'})
+module.exports = withGlobalLess({globalPath: path.join(__dirname, './pages/global.less')})
 ```
